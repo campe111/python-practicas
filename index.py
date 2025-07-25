d72cd01233d675b614cf743f1c8c16b4cdb99a67
Brian <brianmatias999@gmail.com>
2025-07-25 13:39:39 -0300

    Iniciando con el primer comit

---
+++ index.py
@@ -0,0 +1,7 @@
+﻿#verificar si un numero es par o impar
+numero = 7
+numero = int(input("Ingrese un numero: "))
+if numero % 2 == 0;
+print ("El numero es par")
+else:
+    print("El numero es impar")
